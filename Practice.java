3ab4847647572e9bc582df0cc87146d763ef46d1
rdespotovic <37915944+rdespotovic@users.noreply.github.com>
2018-07-31 11:46:49 -0700

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
-public class Cloudera {
+public class Practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
